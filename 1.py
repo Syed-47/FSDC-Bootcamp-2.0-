7f89380b382b837f36a65b3d234ecb7f2be07feb
Syed-47 <115080760+Syed-47@users.noreply.github.com>
2022-10-15 23:16:17 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. In the below elements which of them are values or an expression? eg:- values can be
+# integer or string and expressions will be mathematical operators.
+
+# ########## Answer
+# 
+# 'hello' = value
+# 
+# -87.8 = value
+# 
+# -  =  operator
+# 
+# / = operator
+# 
+# +  = operator
+# 
+# 6 = value
+
+# # 2. What is the difference between string and variable?
+
+# ####### Answer
+# 
+# string will be represented in inverted commas
+# 
+# variable will be not consists of inverted commas
+# 
+# 'Syed' = String
+# 
+# syed = variable
+
+# # Describe three different data types.
+
+# #########  Answer
+# 
+# Three different data types are
+# 
+# String 
+# 
+# Integer
+# 
+# Floating Point
+
+# # What is an expression made up of? What do all expressions do?
+
+# ########## Answer
+# 
+# An expression is a combination of values, variables, operators, and calls to functions. 
+# 
+# Expressions need to be evaluated. If you ask Python to print an expression, the interpreter evaluates the expression and displays the result.
+
+# # 5. This assignment statements, like spam = 10. What is the difference between an
+# expression and a statement?
+
+# ########  Answer
+# 
+# An Expression always evaluates to a value. And, A statement does something, like creating a variable or displaying a value
+# 
+# Example : "1+3" is an expression 
+#            
+#            x = 1+3 is a statement 
+
+# # 6. After running the following code, what does the variable bacon contain?
+# bacon = 22
+# bacon + 1
+
+# In[2]:
+
+
+bacon = 22
+bacon + 1
+
+
+# In[3]:
+
+
+bacon
+
+
+# ###### Answer
+# 
+# bacon still contains value 22
+
+# # 7. What should the values of the following two terms be?
+# 'spam' + 'spamspam'
+# 'spam' * 3
+
+# In[4]:
+
+
+'spam' + 'spamspam' 
+
+
+# In[7]:
+
+
+'spam'*3
+
+
+# ######## Answer
+# 
+# 'spam' + 'spamspam'   =  'spamspamspam'
+# 
+# 'spam'*3  =  'spamspamspam'
+
+# # 8. Why is eggs a valid variable name while 100 is invalid?
+
+# ####### Answer
+# 
+# This is because variable names cannot begin with a number.
+# 
+
+# # 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+
+# ####### Answer
+# 
+# int() for intergets
+# 
+# float() for floating-point
+# 
+# str()  for  string
+
+# # 10. Why does this expression cause an error? How can you fix it?
+# 'I have eaten' + 99 + 'burritos' 
+
+# In[8]:
+
+
+'I have eaten 99 burritos' 
+
+
+# ######## Answer
+# 
+# This expression causes and error because in this line 'I have eaten' and 'burritos' are strings, while 99 is treated as integer. In order to fix the error and print 'I have eaten 99 burritos.', 99 needs '' around it to treat it as a string.
+
+# In[ ]:
+
+
+
+
